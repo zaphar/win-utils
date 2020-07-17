35b841a91dfce0db778a2edb8523c9695895f5cd
Jeremy Wall <jeremy@marzhillstudios.com>
2020-07-16 21:07:06 -0400

    Add opening a query and limit the scope of unsafe

--- winapi-perf-wrapper/src/lib.rs
+++ winapi-perf-wrapper/src/lib.rs
@@ -1,3 +1,4 @@
+use winapi::ctypes::c_void;
 use winapi::shared::minwindef::{DWORD, FALSE, TRUE};
 use winapi::shared::winerror::ERROR_SUCCESS;
 use winapi::um::pdh::{
@@ -47,57 +48,58 @@ impl PDH {
     }
 
     fn enumerate_objects(&mut self) -> Result<Vec<String>, PDHStatus> {
-        unsafe {
-            let data_source = null_mut();
-            let machine_name = if let Some(ref mut machine_name) = self.machine_name {
-                machine_name.as_mut_ptr()
-            } else {
-                null_mut()
-            };
-            let mut buffer_length: DWORD = 0;
-            // The first time we call this to find out what the required buffer
-            // size is.
-            let mut status = PdhEnumObjectsW(
+        let data_source = null_mut();
+        let machine_name = if let Some(ref mut machine_name) = self.machine_name {
+            machine_name.as_mut_ptr()
+        } else {
+            null_mut()
+        };
+        let mut buffer_length: DWORD = 0;
+        // The first time we call this to find out what the required buffer
+        // size is.
+        let mut status = unsafe {
+            PdhEnumObjectsW(
                 data_source,
                 machine_name,
                 null_mut(),
                 &mut buffer_length,
                 PERF_DETAIL_STANDARD,
                 TRUE,
-            ) as u32;
-            if status == constants::PDH_MORE_DATA {
-                // buffer length should be set to the appropriate length.
-                // Now call it a second time to get the list of objects.
-                // This will be filled with a null separated list of names.
-                let mut object_list = Vec::<u16>::with_capacity(buffer_length as usize);
-                object_list.resize(buffer_length as usize, 0);
-                status = PdhEnumObjectsW(
+            )
+        } as u32;
+        if status == constants::PDH_MORE_DATA {
+            // buffer length should be set to the appropriate length.
+            // Now call it a second time to get the list of objects.
+            // This will be filled with a null separated list of names.
+            let mut object_list = Vec::<u16>::with_capacity(buffer_length as usize);
+            object_list.resize(buffer_length as usize, 0);
+            status = unsafe {
+                PdhEnumObjectsW(
                     data_source,
                     machine_name,
                     object_list.as_mut_ptr(),
                     &mut buffer_length,
                     PERF_DETAIL_STANDARD,
                     FALSE,
-                ) as u32;
-                if status == ERROR_SUCCESS {
-                    return Ok(null_separated_to_vec(object_list));
-                } else {
-                    return Err(status);
-                }
+                )
+            } as u32;
+            if status == ERROR_SUCCESS {
+                return Ok(null_separated_to_vec(object_list));
             } else {
-                // Error! we expected more data here.
                 return Err(status);
             }
+        } else {
+            // Error! we expected more data here.
+            return Err(status);
         }
     }
 
-    // TODO relevant error codes for this
     fn enumerate_items(&mut self, obj: &str) -> Result<(Vec<String>, Vec<String>), PDHStatus> {
-        unsafe {
-            let mut object_name = str_to_utf16(&obj);
-            let mut counter_list_len: DWORD = 0;
-            let mut instance_list_len: DWORD = 0;
-            let mut status = PdhEnumObjectItemsW(
+        let mut object_name = str_to_utf16(&obj);
+        let mut counter_list_len: DWORD = 0;
+        let mut instance_list_len: DWORD = 0;
+        let mut status = unsafe {
+            PdhEnumObjectItemsW(
                 null_mut(),
                 null_mut(),
                 object_name.as_mut_ptr(),
@@ -107,11 +109,13 @@ impl PDH {
                 &mut instance_list_len,
                 PERF_DETAIL_STANDARD,
                 0,
-            ) as PDHStatus;
-            if status == constants::PDH_MORE_DATA {
-                let mut counter_list = zeroed_buffer(counter_list_len as usize);
-                let mut instance_list = zeroed_buffer(instance_list_len as usize);
-                status = PdhEnumObjectItemsW(
+            )
+        } as PDHStatus;
+        if status == constants::PDH_MORE_DATA {
+            let mut counter_list = zeroed_buffer(counter_list_len as usize);
+            let mut instance_list = zeroed_buffer(instance_list_len as usize);
+            status = unsafe {
+                PdhEnumObjectItemsW(
                     null_mut(),
                     null_mut(),
                     object_name.as_mut_ptr(),
@@ -121,18 +125,28 @@ impl PDH {
                     &mut instance_list_len,
                     PERF_DETAIL_STANDARD,
                     0,
-                ) as PDHStatus;
-                if status != ERROR_SUCCESS {
-                    return Err(status);
-                }
-                return Ok((
-                    null_separated_to_vec(counter_list),
-                    null_separated_to_vec(instance_list),
-                ));
-            } else {
+                )
+            } as PDHStatus;
+            if status != ERROR_SUCCESS {
                 return Err(status);
             }
+            return Ok((
+                null_separated_to_vec(counter_list),
+                null_separated_to_vec(instance_list),
+            ));
+        } else {
+            return Err(status);
         }
+    }
+
+    pub fn open_query(&mut self) -> Result<PdhQuery, PDHStatus> {
+        let mut query = PdhQuery(null_mut());
+        let status = unsafe { PdhOpenQueryW(null_mut(), 0, query.query()) } as u32;
+
+        if status != ERROR_SUCCESS {
+            return Err(status);
+        }
+        return Ok(query);
     }
 
     pub fn enumerate_counters(&mut self) -> Result<Vec<String>, PDHStatus> {
@@ -163,5 +177,25 @@ impl PDH {
             }
         }
         return Ok(counter_path_vec);
+    }
+}
+
+pub struct PdhQuery(HQuery);
+
+impl PdhQuery {
+    pub fn query(&mut self) -> &mut HQuery {
+        &mut self.0
+    }
+
+    // TODO Add counter?
+
+    // TODO Counter data interator?
+}
+
+impl Drop for PdhQuery {
+    fn drop(&mut self) {
+        unsafe {
+            PdhCloseQuery(self.0);
+        }
     }
 }
